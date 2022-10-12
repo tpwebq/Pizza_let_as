65b8d84c5db7794305ed087fbf0a3f200a61b6fa
TP <pankowski.tomasz@prokonto.pl>
2022-10-12 20:17:45 +0100

    layout

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,21 @@
+import Basket from "./views/basket";
 import Comingsoon from "./views/comingsoon";
+import Contact from "./views/contact";
 import Error from "./views/error";
+import Index from "./views/index";
+import Pizza from "./views/pizza";
 import Privacy from "./views/privacy";
+import Promo from "./views/promo";
 
 const ROUTES = [
-    {path: "/",component:<Comingsoon/>},
-    {path: "/privacy",component:<Privacy/>},
-    {path:"*",component:<Error/>}
+    {navbarItem:true, name:"Home", path: "/",component:<Index/>},
+    {navbarItem:true, name:"Pizza", path: "/pizza",component:<Pizza/>},
+    {navbarItem:true, name:"Promo", path: "/promo",component:<Promo/>},
+    {navbarItem:true, name:"Contact", path: "/contact",component:<Contact/>},
+    {navbarItem:true, name:"Basket", path: "/basket",component:<Basket/>},
+    {navbarItem:false, name:"Comingsoon", path: "/comingsoon",component:<Comingsoon/>},
+    {navbarItem:false, name:"Privacy", path: "/privacy",component:<Privacy/>},
+    {navbarItem:false, path:"*",component:<Error/>}
 ];
 
 export {
---
+++ src/components/pizza-s1.js
@@ -0,0 +1,8 @@
+
+function PizzaS1(){
+    return(
+        <section className="pizza-s1 minh-25vh"></section>
+    );
+}
+
+export default PizzaS1;
